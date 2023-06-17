d1b7b27b903ada1a5764f998c524e1269479d034
呂 博多 <blyu@ryonoMacBook-Air.local>
2023-06-17 15:00:36 +0900

    sql add index 0.2

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -1,8 +1,4 @@
 ALTER TABLE match_group_member ADD INDEX user_id_idx(user_id);
-ALTER TABLE match_group_member ADD INDEX role_id_idx(role_id);
-ALTER TABLE match_group_member ADD INDEX belong_idx(belong);
-ALTER TABLE department ADD INDEX department_id_idx(department_id);
-ALTER TABLE department_role_member ADD INDEX user_id_idx(user_id);
-ALTER TABLE department_role_member ADD INDEX belong_idx(belong);
-ALTER TABLE user ADD INDEX mail_idx(mail);
-ALTER TABLE user ADD INDEX password_idx(password);
+ALTER TABLE department_role_member ADD INDEX role_id_belong_idx(role_id, belong);
+ALTER TABLE department_role_member ADD INDEX user_id_belong_idx(user_id, belong);
+ALTER TABLE user ADD INDEX mail_password_idx(mail, password);
